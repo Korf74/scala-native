780cd4d635259324b706382614bb9ac1379467ec
Remi Coudert <remi.coudert@epfl.ch>
2017-07-07 16:16:36 +0200

    dd

--- sandbox/Test.scala
+++ sandbox/Test.scala
@@ -1,5 +1,10 @@
+import scalanative._
+import native._
+
 object Test {
   def main(args: Array[String]): Unit = {
-    println("Hello, World!")
+    def f(a: Ptr[Byte]) = a
+
+    val b = CFunctionPtr.fromFunction1(f)
   }
 }
