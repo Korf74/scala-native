e11eac92ae3783672e7b3c91e97175ebffb13bfa
Remi Coudert <remi.coudert@epfl.ch>
2017-08-15 12:23:06 +0200

    fefw

--- nativelib/src/main/scala/scala/scalanative/runtime/Unsafe/Unsafe.scala
+++ nativelib/src/main/scala/scala/scalanative/runtime/Unsafe/Unsafe.scala
@@ -3,16 +3,16 @@ package runtime.Unsafe
 
 object Unsafe {
 
-  //import NativeUnsafe._
+  import NativeUnsafe._
 
   private val lock: Object = new Object()
 
-  /*def compareAndSwapObject(obj: Object, expected: Object, update: Object): Boolean = lock.synchronized {
+  def compareAndSwapObject(obj: Object, expected: Object, update: Object): Boolean = lock.synchronized {
     if(expected != obj) return false
 
     update_object(obj, update)
     true
-  }*/
+  }
 
   def park(thread: Thread): Unit = thread.suspend()
 
