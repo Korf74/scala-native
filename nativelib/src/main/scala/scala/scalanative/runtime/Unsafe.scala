6e2e2814a5e2e1c8642bb41787a0ebe22085309f
Remi Coudert <remi.coudert@epfl.ch>
2017-08-16 21:36:49 +0200

    sdf

--- nativelib/src/main/scala/scala/scalanative/runtime/Unsafe.scala
+++ nativelib/src/main/scala/scala/scalanative/runtime/Unsafe.scala
@@ -1,31 +1,22 @@
 package scala.scalanative
 package runtime
 
-import native.{extern, name, CInt}
+import native.{extern, name, CInt, stackalloc, CLong, Ptr}
 import posix.sys.types.pthread_t
 
 object Unsafe {
 
+  private val atomicObject = CAtomicRef[Object]()
+
   import NativeUnsafe._
 
   private val lock: Object = new Object()
-
-  def compareAndSwapObject(obj: Object, expected: Object, update: Object): Boolean = lock.synchronized {
-    if (!expected.equals(update)) return false
-
-    updateObject(obj, update)
-    true
-  }
 
   def park(thread: Thread): Unit = thread.suspend()
 
   def unpark(thread: Thread): Unit = thread.resume()
 
   @extern
-  private object NativeUnsafe {
+  private object NativeUnsafe {}
 
-    @name("update_object")
-    def updateObject(obj: Object, update: Object): Unit = extern
-
-  }
 }
